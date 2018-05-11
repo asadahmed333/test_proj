ba4286dff5b05ed77c6a47624165cb2451143a5b
asadahmed333 <asadnaem1@gmail.com>
2018-05-11 05:24:24 -0400

    added partials and delete method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,13 @@ end
 @article = Article.find(params[:id])
  end
 
+ def destroy
+  @article = Article.find(params[:id])
+@article.destroy
+flash[:notice] = "article was successfully deleted"
+redirect_to articles_path
+ end
+
  def edit
 @article = Article.find(params[:id])
  end

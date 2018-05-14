e423f5e58583612ae669cfc01ba37b05cd996109
asadahmed333 <asadnaem1@gmail.com>
2018-05-14 05:01:47 -0400

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
 
+validates :username, presence: true, uniqueness: {case_sensitive: false} , length: {minimum: 3, maximum: 25}
 
+VALID_EMAI_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+validates :email, presence: true, uniqueness: {case_sensitive: false} , length: {minimum: 5, maximum: 15},
+
+format: {with: VALID_EMAI_REGEX}
 end

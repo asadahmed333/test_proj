253841973c39e7a6260eeeb0da284b7247110738
asadahmed333 <asadnaem1@gmail.com>
2018-05-14 07:21:07 -0400

    edit module for user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,24 @@ else
 end
 end
 
+def edit
+@user = User.find(params[:id])
+end
+
+def update
+@user = User.find(params[:id])
+
+if @user.update(user_params)
+
+  flash[:success] = "your account has been edited successfully "
+  redirect_to articles_path
+else
+
+    render 'edit'
+
+end
+end
+
 
 
 private
